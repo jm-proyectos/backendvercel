d18a8f76d242ef3025bbf6e1e1e9ad3577148fe2
Jhonny <jhonnyalbertomo.com>
2025-11-16 06:04:58 -0400

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -87,6 +87,7 @@ export const logout = async (req, res) => {
 
 
 export async function createAccessToken(payload) {
+  const TOKEN_SECRET = process.env.TOKEN_SECRET;
   return new Promise((resolve, reject) => {
     jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
       if (err) reject(err);
